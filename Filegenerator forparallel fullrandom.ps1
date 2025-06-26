fc4ed2fae9474d366a26f2579ee371a6fb5b134d
Erik Johnson <erik.johnson@tttech.com>
2025-06-26 11:27:00 +0200

    Move chunks

--- Filegenerator forparallel fullrandom.ps1
+++ Filegenerator forparallel fullrandom.ps1
@@ -5,15 +5,14 @@ $work=@{
 	errorTypes=@('Sandextrator overload','Conveyor misalignment','Valve stuck','Temperature warning')
 	statusCodes=@('OK','WARN','ERR')
 
-	chunks=@((0..9999),(10000..19999),(20000..29999),(30000..39999),(40000..49999))
 	output=[String[]]::new(50000)	#Pre-allocate output array, then index it.  No speed diff noticed vs [System.Collections.ArrayList].Add()
 	date=Get-Date	#Prefetch the date once
 	random=New-Object System.Random	#Faster than Get-Random
 }
 
-0..4 | ForEach-Object -Parallel {
+@((0..9999),(10000..19999),(20000..29999),(30000..39999),(40000..49999)) | ForEach-Object -Parallel {
 	$work=$using:work
-	ForEach ($i in $work.chunks[$_]){
+	ForEach ($i in $_){
 		$timestamp=$work.date.AddSeconds(-$i).ToString("yyyy-MM-dd HH:mm:ss")	#IDK what to speed up here
 		$plc=$work.plcNames[$work.random.Next()%4]
 		$operator=$work.random.Next()%20+101
--- Filegenerator forparallel.ps1
+++ Filegenerator forparallel.ps1
@@ -5,15 +5,15 @@ $work=@{
 	errorTypes=@('Sandextrator overload','Conveyor misalignment','Valve stuck','Temperature warning')
 	statusCodes=@('OK','WARN','ERR')
 
-	chunks=@((0..9999),(10000..19999),(20000..29999),(30000..39999),(40000..49999))
 	output=[String[]]::new(50000)	#Pre-allocate output array, then index it.  No speed diff noticed vs [System.Collections.ArrayList].Add()
 	date=Get-Date	#Prefetch the date once
 	random=New-Object System.Random	#Faster than Get-Random
 }
 
-0..4 | ForEach-Object -Parallel {
+# $c=50000,5;$cs=$c[0]/$c[1];$(ForEach($i in 0..($c[1]-1)){"($($i*$cs)..$($i*$cs+$cs-1))"}) -Join ","
+@((0..9999),(10000..19999),(20000..29999),(30000..39999),(40000..49999) | ForEach-Object -Parallel {
 	$work=$using:work
-	ForEach ($i in $work.chunks[$_]){
+	ForEach ($i in $_){
 		$seed=$work.random.Next()	#Get a random number for further pseudo-pseudo random extractions.  Output is still random.
 		$timestamp=$work.date.AddSeconds(-$i).ToString("yyyy-MM-dd HH:mm:ss")	#IDK what to speed up here
 		$plc=$work.plcNames[$seed%4]
